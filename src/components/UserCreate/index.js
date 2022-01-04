ae129ecb73750a37e2db8c3be8ef8d3e3dafe28a
hiago-ramalho <hiagosantosramalho@gmail.com>
2022-01-04 08:59:42 -0300

    Criando hook use Fetch e inicializando o uso.

--- src/components/UserCreate/index.js
+++ src/components/UserCreate/index.js
@@ -1,28 +1,32 @@
 import { useContext } from 'react';
-import {UserContext} from '../../context/UserContext';
+import { UserContext } from '../../context/UserContext';
+import { USER_POST } from '../../services/api';
 import useForm from '../../hooks/useForm';
+import useFetch from '../../hooks/useFetch';
+
 import Input from '../Input';
 import Button from '../Button';
-import { USER_POST } from '../../services/api';
+import Error from '../Helper/Error';
 
 export default function UserCreate() {
   const username = useForm();
   const email = useForm('email');
   const password = useForm();
 
-  const {userLogin} = useContext(UserContext);
+  const { userLogin } = useContext(UserContext);
+  const { loading, error, request } = useFetch();
 
   async function handleSubmit(event) {
     event.preventDefault();
 
-    const {url, options} = USER_POST({
+    const { url, options } = USER_POST({
       username: username.value,
       email: email.value,
       password: password.value
     });
 
-    const response = await fetch(url, options);
-    
+    const { response } = await request(url, options);
+
     if (response.ok) userLogin(username.value, password.value);
 
     console.log(response);
@@ -36,7 +40,11 @@ export default function UserCreate() {
         <Input label="Usuário" type="text" name="username" {...username} />
         <Input label="E-mail" type="text" name="email" {...email} />
         <Input label="Senha" type="password" name="password" {...password} />
-        <Button>Cadastrar</Button>
+
+        {loading ? <Button disabled >Cadastrando...</Button> : <Button>Cadastrar</Button>}
+
+        <Error error={error} />
+
       </form>
     </section>
   )
