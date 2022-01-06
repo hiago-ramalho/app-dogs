f6c36751d3da8ba62a2ab069e790a4e4a05a9c2f
hiago-ramalho <hiagosantosramalho@gmail.com>
2022-01-06 15:38:51 -0300

    Criando o hook useMedia para implementação do menu mobile./ Adicionndo ao componente UserHeaderNav o menu mobile(lógica e estilos).

--- src/components/UserHeaderNav/index.js
+++ src/components/UserHeaderNav/index.js
@@ -1,6 +1,7 @@
-import { useContext, useState } from "react";
-import { NavLink } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { NavLink, useLocation } from "react-router-dom";
 
+import useMedia from "../../hooks/useMedia";
 import { UserContext } from '../../context/UserContext';
 
 import { ReactComponent as FeedSvg } from '../../assets/feed.svg';
@@ -11,33 +12,57 @@ import { ReactComponent as LogoutSvg } from '../../assets/sair.svg';
 import styles from './UserHeaderNav.module.css';
 
 export default function UserHeaderNav() {
-  const [mobile, setMobile] = useState(null);
   const { userLogout } = useContext(UserContext);
 
+  const mobile = useMedia('(max-width: 40rem)');
+
+  const [mobileMenu, setMobileMenu] = useState(false);
+
+  const { pathname } = useLocation();
+  useEffect(() => {
+    setMobileMenu(false);
+  }, [pathname]);
+
   return (
-    <nav className={styles.nav}>
-      <NavLink to="/account" end >
-        <FeedSvg />
-        {mobile && 'Minhas Fotos'}
-      </NavLink>
-      <NavLink to="/account/statistics" >
-        <StatisticSvg />
+    <>
+      {mobile && (
+        <button
+          className={`${styles.mobileButton} ${mobileMenu && styles.mobileButtonActive}`}
+          aria-label="Menu"
+          onClick={() => setMobileMenu(!mobileMenu)}
+        />
+      )}
+      <nav
+        className={
+          `
+            ${mobile ? styles.navMobile : styles.nav}
+            ${mobileMenu && styles.navMobileActive}
+          `
+        }
+      >
+        <NavLink to="/account" end >
+          <FeedSvg />
+          {mobile && 'Minhas Fotos'}
+        </NavLink>
+        <NavLink to="/account/statistics" >
+          <StatisticSvg />
 
-        {mobile && 'Estatísticas'}
+          {mobile && 'Estatísticas'}
 
-      </NavLink>
-      <NavLink to="/account/post">
-        <AddSvg />
+        </NavLink>
+        <NavLink to="/account/post">
+          <AddSvg />
 
-        {mobile && 'Adicionar Foto'}
+          {mobile && 'Adicionar Foto'}
 
-      </NavLink>
+        </NavLink>
 
-      <button onClick={userLogout}>
-        <LogoutSvg />
-        {mobile && 'Sair'}
+        <button onClick={userLogout}>
+          <LogoutSvg />
+          {mobile && 'Sair'}
 
-      </button>
-    </nav>
+        </button>
+      </nav>
+    </>
   )
 }
